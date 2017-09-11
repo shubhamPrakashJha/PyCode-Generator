63f2361d5aed8ab1d731c1e003a152ba3be0c14b
shubhamPrakashJha <sjonline4u@gmail.com>
2017-09-11 23:47:50 +0530

    simplified code

--- codeMaker.py
+++ codeMaker.py
@@ -1,42 +1,42 @@
-def add():
-    codeInString = 'a={0}\nb={1}\nsum=a+b\nprint("sum =",sum)'.format(a,b)
+def calc(codeInString):
     print(codeInString)
-    codeObejct = compile(codeInString,'code', 'exec')
+    codeObejct = compile(codeInString, 'code', 'exec')
     print("-------------Output---------------")
     exec(codeObejct)
+
+def add():
+    codeInString = 'a={0}\nb={1}\nsum=a+b\nprint("sum =",sum)'.format(a,b)
+    calc(codeInString)
 
 def sub():
     codeInString = 'a={0}\nb={1}\nsum=a-b\nprint("sum =",sum)'.format(a,b)
-    print(codeInString)
-    codeObejct = compile(codeInString,'code', 'exec')
-    print("-------------Output---------------")
-    exec(codeObejct)
+    calc(codeInString)
 
 def mul():
     codeInString = 'a={0}\nb = {1}\nsum = a*b\nprint("sum =",sum)'.format(a,b)
-    print(codeInString)
-    codeObejct = compile(codeInString,'code', 'exec')
-    print("-------------Output---------------")
-    exec(codeObejct)
+    calc(codeInString)
 
 def div():
     codeInString = 'a={0}\nb={1}\nsum=a/b\nprint("sum =",sum)'.format(a,b)
-    print(codeInString)
-    codeObejct = compile(codeInString,'code', 'exec')
-    print("-------------Output---------------")
-    exec(codeObejct)
+    calc(codeInString)
+
+def pow():
+    codeInString = 'a={0}\nb={1}\nsum=a**b\nprint("sum =",sum)'.format(a,b)
+    calc(codeInString)
 
 a = int(input("enter first number: "))
 b = int(input("enter second number: "))
 print("choose any option from: ")
-print(" add\t sub\t mul\t div")
+print(" add\t sub\t mul\t div\t pow")
 func = input("enter the function: ")
 print("=============Code=================")
 if func == "add":
     add()
 elif func =="sub":
     sub()
-if func == "mul":
+elif func == "mul":
     mul()
 elif func =="div":
     div()
+elif func == "pow":
+    pow()
